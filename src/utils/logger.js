c1f89bbcf5e151c6c04a05db6457256b6ea6a614
Chamikara Nayanajith <18645565+ChamikaraN@users.noreply.github.com>
2023-05-18 07:19:04 +0530

    1st submition issues fixed

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,43 @@
 const { format, createLogger, transports } = require("winston");
 const { combine, timestamp, label, printf, prettyPrint } = format;
 const DailyRotateFile = require("winston-daily-rotate-file");
+const { MongoDB } = require("winston-mongodb");
 const CATEGORY = "EmpMngSys";
+
+let loggerTransports = [
+  new transports.Console(),
+  new DailyRotateFile({
+    level: "error",
+    filename: "logs/error-%DATE%.log",
+    datePattern: "YYYY-MM-DD",
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+  }),
+  new DailyRotateFile({
+    level: "debug",
+    filename: "logs/debug-%DATE%.log",
+    datePattern: "YYYY-MM-DD",
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+  }),
+];
+
+// Check if running in production
+if (process.env.NODE_ENV === "production") {
+  loggerTransports = [
+    new MongoDB({
+      level: "debug",
+      db: process.env.MONGODB_URL,
+      options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      collection: "logs",
+    }),
+  ];
+}
 
 const logger = createLogger({
   level: "debug",
@@ -12,25 +48,7 @@ const logger = createLogger({
     }),
     prettyPrint()
   ),
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      level: "error",
-      filename: "logs/error-%DATE%.log",
-      datePattern: "YYYY-MM-DD",
-      zippedArchive: true,
-      maxSize: "20m",
-      maxFiles: "14d",
-    }),
-    new DailyRotateFile({
-      level: "debug",
-      filename: "logs/debug-%DATE%.log",
-      datePattern: "YYYY-MM-DD",
-      zippedArchive: true,
-      maxSize: "20m",
-      maxFiles: "14d",
-    }),
-  ],
+  transports: loggerTransports,
 });
 
 module.exports = logger;
