996b4151a3880c645cc8089be890220db92b3092
Chamikara Nayanajith <18645565+ChamikaraN@users.noreply.github.com>
2023-06-12 09:32:52 +0530

    small changers done

--- src/middleware/validateEmployee.ts
+++ src/middleware/validateEmployee.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import { validationRules } from "../utils/validationRules";
 
 function validateInput(
   req: Request,
@@ -7,18 +6,6 @@ function validateInput(
   next: NextFunction
 ): boolean {
   const errors: { [key: string]: string } = {};
-
-  // for (const [key, value] of Object.entries(req.body)) {
-  //   const rule = validationRules[key];
-
-  //   if (rule) {
-  //     if (rule.required && !value) {
-  //       errors[key] = `${key} is required`;
-  //     } else if (value && !rule.pattern.test(value)) {
-  //       errors[key] = rule.message;
-  //     }
-  //   }
-  // }
 
   // return validation errors if any
   if (Object.keys(errors).length > 0) {
